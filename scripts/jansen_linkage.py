de0958ee4c869c805419c1833270c476e3f9d278
Lalit <jayanti.lalit@gmail.com>
2024-04-12 12:05:55 +0530

    separated utils and main program, code clean up

---
+++ scripts/jansen_linkage.py
@@ -0,0 +1,150 @@
+import matplotlib.animation
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tom_utils import fourBar
+from tom_utils import Point
+
+class jansenLinkage():
+    
+    def __init__(self):
+        
+        self.link = {"b":41.5, "c":39.3,
+                     "d":40.1, "e":55.8, "f":39.4,
+                     "g":36.7, "h":65.7, "i":49.0,
+                     "j":50.0, "k":61.9, "n":(38.0**2 + 7.8**2)**0.5,
+                     "m":15.0}
+        
+        self.tn = np.arctan2(7.8, 38.0)
+
+        # links=  {'b': 41.5,
+        #          'c': 39.2999999211865,
+        #          'd': 20,
+        #          'e': 46.06788469204984,
+        #          'f': 40.713973766257695,
+        #          'g': 25.763225634581385,
+        #          'h': 58.58107181426522,
+        #          'i': 48.99999998099539,
+        #          'j': 50,
+        #          'k': 51.5720683758581,
+        #          'n': 29.51694859289377,
+        #          'm': 14.300767286373192}
+        
+        # self.link = links
+        # self.tn = 0.0
+
+            
+    
+    def kinematic_analysis(self, tm, wm=0, wn=0, am=0, an=0):
+
+        tn = self.tn
+        
+        ## eqn 1
+        tj, tb, wj, wb, aj, ab = fourBar().solver(self.link["n"], self.link["m"],
+                                                 self.link["j"], self.link["b"],
+                                                 self.tn, tm,
+                                                 wn, wm,
+                                                 an, am,
+                                                 mode=0)
+        ## eqn 2
+        tk, tc, wk, wc, ak, ac = fourBar().solver(self.link["n"], self.link["m"],
+                                                 self.link["k"], self.link["c"],
+                                                 self.tn, tm,
+                                                 wn, wm,
+                                                 an, am,
+                                                 mode=1)
+        ## eqn 4
+        td = tb + np.arccos((self.link["b"]**2+self.link["d"]**2-self.link["e"]**2)/(2*self.link["b"]*self.link["d"])) + np.pi
+        wd = wb
+        ad = ab
+        
+        ## eqn 3
+        tg, tf, wg, wf, ag, af = fourBar().solver(self.link["d"], self.link["c"],
+                                                 self.link["g"], self.link["f"],
+                                                 td, tc,
+                                                 wd, wc,
+                                                 ad, ac,
+                                                 mode=1)
+        ## eqn 5
+        ti = tg + np.arccos((self.link["g"]**2+self.link["i"]**2-self.link["h"]**2)/(2*self.link["g"]*self.link["i"])) - np.pi
+        wi = wg
+        wh = wg
+        ai = ag
+        ah = ag
+        
+        ## eqn 6
+        px = self.link["c"]*np.cos(tc) + self.link["i"]*np.cos(ti)
+        py = self.link["c"]*np.sin(tc) + self.link["i"]*np.sin(ti)
+
+        pos = { "tb":tb, "tc":tc, "td":td,
+                "tf":tf, "tg":tg, "ti":ti,
+                "tj":tj, "tk":tk, "tn":self.tn,
+                "tm":tm, "px":px, "py":py}
+        
+        ang_vel = { "wb":wb, "wc":wc, "wd":wd,
+                "wf":wf, "wg":wg, "wi":wi,
+                "wj":wj, "wk":wk, "wn":wn,
+                "wm":wm}
+        
+        ang_acc = { "ab":ab, "ac":ac, "ad":ad,
+                "af":af, "ag":ag, "ai":ai,
+                "aj":aj, "ak":ak, "an":an,
+                "am":am}
+
+        # declare points for kinematic analysis
+        vecs = {}
+        for ci in ["b", "c", "d", "f", "g", "i", "j", "k", "n", "m"]:
+            vecs[ci] = Point(self.link[ci], pos[f"t{ci}"], ang_vel[f"w{ci}"], ang_acc[f"a{ci}"])
+
+        points = {"p0":Point(0,0,0,0),      "p3":vecs["n"],
+                  "p1":vecs["n"]+vecs["m"], "p2":vecs["b"],
+                  "p7":vecs["c"]+vecs["i"], "p5":vecs["c"],
+                  "p6":vecs["c"]-vecs["g"], "p4":-vecs["d"]}
+
+        return pos, ang_vel, ang_acc, points
+        
+    def plot_jansen_linkage(self, idx):
+        plt.cla()
+        res, _, _, _ = self.kinematic_analysis(2*idx*np.pi/180)
+
+        fourBar().plot(self.link["n"], self.link["m"],
+                      self.link["j"], self.link["b"],
+                      res["tn"], res["tm"], res["tj"], res["tb"])
+        
+        fourBar().plot(self.link["n"], self.link["m"],
+                      self.link["k"], self.link["c"],
+                      res["tn"], res["tm"], res["tk"], res["tc"])
+        
+        fourBar().plot(self.link["d"], self.link["c"],
+                      self.link["g"], self.link["f"],
+                      res["td"], res["tc"], res["tg"], res["tf"],
+                      ox = -(self.link["d"]*np.cos(res["td"])),
+                      oy = -(self.link["d"]*np.sin(res["td"])))
+        
+        plt.plot([self.link["c"]*np.cos(res["tc"]),res["px"]],
+                 [self.link["c"]*np.sin(res["tc"]),res["py"]])
+
+        plt.plot([self.link["c"]*np.cos(res["tc"])-self.link["g"]*np.cos(res["tg"]),res["px"]],
+                 [self.link["c"]*np.sin(res["tc"])-self.link["g"]*np.sin(res["tg"]),res["py"]])
+        
+        plt.plot([self.link["b"]*np.cos(res["tb"]), -self.link["d"]*np.cos(res["td"])],
+                 [self.link["b"]*np.sin(res["tb"]), -self.link["d"]*np.sin(res["td"])])
+        
+        plt.plot(res["px"], res["py"], "ro")
+        
+        
+        
+    def display(self):
+        
+        self.fig, self.ax = plt.subplots(figsize=(10,5))
+        self.animation = matplotlib.animation.FuncAnimation(self.fig, 
+                                           self.plot_jansen_linkage, 
+                                           frames=180)
+        
+        writer = matplotlib.animation.PillowWriter(fps=72)
+        self.animation.save("../outputs/jansen_linkage.gif", writer=writer)
+
+
+if __name__ == "__main__": 
+    pass
+        
---
+++ scripts/tom_utils.py
@@ -0,0 +1,228 @@
+import matplotlib.animation
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class fourBar():
+
+    def __init__(self):
+        pass
+
+    def pos_solver(self, a, b, c, d,
+                            ta, tb, mode=0):
+
+        x = a*np.cos(ta) + b*np.cos(tb)
+        y = a*np.sin(ta) + b*np.sin(tb)
+
+        R = np.sqrt(x**2 + y**2)
+
+        Ac = (c**2 - d**2 + R**2)/(2*c)
+        Ad = (d**2 - c**2 + R**2)/(2*d)
+
+        tc = 2*np.arctan2(- y + ((-1)**mode)*np.sqrt(R**2 - Ac**2), -x-Ac)
+        td = 2*np.arctan2(- y - ((-1)**mode)*np.sqrt(R**2 - Ad**2), -x-Ad)
+
+        return tc, td
+
+    def vel_solver(self, a, b, c, d,
+                            ta, tb, tc, td,
+                            wa, wb):
+
+        A = np.array([[c*np.cos(tc), d*np.cos(td)],
+                      [c*np.sin(tc), d*np.sin(td)]])
+
+        y = np.array([a*wa*np.cos(ta)+b*wb*np.cos(tb),
+                      a*wa*np.sin(ta)+b*wb*np.sin(tb)])
+
+        wc, wd = np.linalg.solve(A, y.T)
+        return wc, wd
+
+    def acc_solver(self, a, b, c, d,
+                            ta, tb, tc, td,
+                            wa, wb, wc, wd,
+                            aa, ab):
+
+        A = np.array([[c*np.cos(tc), d*np.cos(td)],
+                      [c*np.sin(tc), d*np.sin(td)]])
+
+        y = np.array([(a*aa*np.cos(ta) - a*(wa**2)*np.sin(ta) + c*(wc**2)*np.sin(tc)
+                       + b*ab*np.cos(tb) - b*(wb**2)*np.sin(tb) + d*(wd**2)*np.sin(td)),
+                      (a*aa*np.sin(ta) + a*(wa**2)*np.cos(ta) - c*(wc**2)*np.cos(tc)
+                     + b*ab*np.sin(tb) + b*(wb**2)*np.cos(tb) - d*(wd**2)*np.cos(td))])
+
+        ac, ad = np.linalg.solve(A, y.T)
+        return ac, ad
+
+    def solver(self, a, b, c, d,
+                        ta, tb,
+                        wa, wb,
+                        aa, ab,
+                        mode=0):
+
+        tc, td = self.pos_solver(a, b, c, d,
+                                 ta, tb, mode=mode)
+
+        wc, wd = self.vel_solver(a, b, c, d,
+                                 ta, tb, tc, td,
+                                 wa, wb)
+
+        ac, ad = self.acc_solver(a, b, c, d,
+                                 ta, tb, tc, td,
+                                 wa, wb, wc, wd,
+                                 aa, ab)
+
+        return tc, td, wc, wd, ac, ad
+
+    def plot(self, a, b, c, d,
+                      ta, tb, tc, td,
+                      ox=0, oy=0):
+
+        plt.xlim([-80, 60])
+        plt.ylim([-100, 60])
+        plt.gca().set_aspect('equal')
+
+        plt.grid("on")
+        plt.plot([ox+0, ox+a*np.cos(ta)],
+                 [oy+0, oy+a*np.sin(ta)])
+
+        plt.plot([ox+a*np.cos(ta), ox+a*np.cos(ta)+b*np.cos(tb)],
+                 [oy+a*np.sin(ta), oy+a*np.sin(ta)+b*np.sin(tb)])
+
+        plt.plot([ox+0, ox+d*np.cos(td)],
+                 [oy+0, oy+d*np.sin(td)])
+
+        plt.plot([ox+d*np.cos(td), ox+d*np.cos(td)+c*np.cos(tc)],
+                 [oy+d*np.sin(td), oy+d*np.sin(td)+c*np.sin(tc)])
+
+
+class Point():
+    def __init__(self, a=0, ta=0, wa=0, aa=0):
+
+        self.pos = np.array([a*np.cos(ta), a*np.sin(ta)])
+        self.vel = np.array([-a*wa*np.sin(ta), a*wa*np.cos(ta)])
+        self.acc = np.array([-a*(wa**2)*np.cos(ta) - a*aa*np.sin(ta),
+                             -a*(wa**2)*np.sin(ta) + a*(aa)*np.cos(ta)])
+
+    def __add__(self, b):
+        c = Point()
+        c.pos = self.pos + b.pos
+        c.vel = self.vel + b.vel
+        c.acc = self.acc + b.acc
+        return c
+
+    def __sub__(self, b):
+        c = Point()
+        c.pos = self.pos - b.pos
+        c.vel = self.vel - b.vel
+        c.acc = self.acc - b.acc
+        return c
+
+    def __neg__(self):
+        c = Point()
+        c.pos = - self.pos
+        c.vel = - self.vel
+        c.acc = - self.acc
+        return c
+
+    def __repr__(self):
+        str = f"__name__\n"
+        str += f"Position    : {self.pos}\n"
+        str += f"Velocity    : {self.vel}\n"
+        str += f"Acceleration: {self.acc}\n"
+        return str
+
+
+def freudenstein_method(phi, psi, r1):
+
+    # psi = tht4, phi = tht2
+    
+    # k0 - k2 c_tht4 + k4 c_tht2 = c_(tht4-tht2)
+    
+    A = np.array([[1, -np.cos(psi[0]), np.cos(phi[0])],
+                  [1, -np.cos(psi[1]), np.cos(phi[1])],
+                  [1, -np.cos(psi[2]), np.cos(phi[2])]])
+    
+    b = np.array([np.cos(phi[0]-psi[0]),
+                  np.cos(phi[1]-psi[1]),
+                  np.cos(phi[2]-psi[2])])
+    
+    k0, k2, k4 = np.linalg.solve(A, b)
+    
+    
+    r2 = r1/k2
+    r4 = r1/k4
+    r3 = np.sqrt(r1**2 + r2**2 + r4**2 - 2*r2*r4*k0)
+
+    return r1, r2, r3, r4
+
+def d_fn(g1, g2, d1, d2):
+
+    # | g1 d1 |
+    # | g2 d2 |
+    
+    D = np.array([   (np.cos(g1)-1)*d2[0] - d2[1]*np.sin(g1)
+                   -((np.cos(g2)-1)*d1[0] - d1[1]*np.sin(g2)),
+                     (np.cos(g1)-1)*d2[1] + d2[0]*np.sin(g1)
+                   -((np.cos(g2)-1)*d1[1] + d1[0]*np.sin(g2))])
+    return D
+    
+def burmester_method(gam, delta, phi2, mode=0, debug=False):
+
+    # delta[0] = delta_2 ... delta[2] = delta_4
+    # gam[0] = gamma_2 ... gam[2] = gamma_4
+    # phi[0] = phi_2 ... phi[2] = phi_4
+    
+    D2 =  d_fn(gam[1], gam[2], delta[1], delta[2])
+    D3 = -d_fn(gam[0], gam[2], delta[0], delta[2])
+    D4 =  d_fn(gam[0], gam[1], delta[0], delta[1])
+    
+    D1 = -D2 -D3 -D4
+    D = D1 + D2@np.array([[ np.cos(phi2), np.sin(phi2)],
+                          [-np.sin(phi2), np.cos(phi2)]])
+
+
+    [nD, nD1, nD2, nD3, nD4] = [np.linalg.norm(di) for di in [D, D1, D2, D3, D4]]
+
+    if(((nD+nD3)>nD4) and ((nD4+nD)>nD3) and ((nD3+nD4)>nD)):
+        pass
+    else:
+        if debug:
+            print(f"ERROR: no solution for phi3, phi4, given phi2 {phi2}")
+        return np.array([np.nan, np.nan]), np.array([np.nan, np.nan]), np.array([np.nan, np.nan, np.nan])
+        
+    c_tht3 = -(nD3**2 + nD**2 - nD4**2)/(2*nD3*nD)    
+    c_tht4 = -(nD4**2 + nD**2 - nD3**2)/(2*nD4*nD)
+    
+    tht3 = ((-1)**(mode))*np.arccos(c_tht3)
+    tht4 = ((-1)**(mode))*np.arccos(c_tht4)
+
+    phi3 = np.arctan2(D[1], D[0]) - np.arctan2(D3[1], D3[0]) + tht3
+    phi4 = np.arctan2(D[1], D[0]) - np.arctan2(D4[1], D4[0]) - tht4
+
+    phi = np.array([phi2, phi3, phi4])
+
+    A = np.array([[np.cos(phi2)-1, -np.sin(phi2),   np.cos(gam[0])-1, -np.sin(gam[0])],
+                  [np.sin(phi2)  ,  np.cos(phi2)-1, np.sin(gam[0])  ,  np.cos(gam[0])-1],
+                  [np.cos(phi3)-1, -np.sin(phi3),   np.cos(gam[1])-1, -np.sin(gam[1])],
+                  [np.sin(phi3)  ,  np.cos(phi3)-1, np.sin(gam[1])  ,  np.cos(gam[1])-1]])
+
+    b = np.array([delta[0,0], 
+                  delta[0,1],
+                  delta[1,0], 
+                  delta[1,1]])
+
+    try:
+        z1x, z1y, z2x, z2y = np.linalg.solve(A, b)
+    except:
+        if debug:
+            print(f"ERROR: Singular matrix for phi2 {phi2}")
+        return np.array([np.nan, np.nan]), np.array([np.nan, np.nan]), np.array([np.nan, np.nan, np.nan])
+        
+
+    z1 = np.array([z1x, z1y])
+    z2 = np.array([z2x, z2y])
+
+    return z1, z2, phi 
+
+if __name__ == "__main__": 
+    pass
